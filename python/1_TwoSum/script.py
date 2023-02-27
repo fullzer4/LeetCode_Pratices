cf006f212301108de0c9b38a3320e8053fc47aa4
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-27 09:31:08 -0300

    new

---
+++ python/1_TwoSum/script.py
@@ -0,0 +1,5 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]: int
+        
+        
+Solution.twoSum([2,7,11,15], 9)
---
+++ typescript/1_TwoSum/index.ts
@@ -0,0 +1,9 @@
+
+function twoSum(nums: number[], target: number): number[] {
+
+
+
+    return []
+};
+
+twoSum([2,7,11,15], 9)
