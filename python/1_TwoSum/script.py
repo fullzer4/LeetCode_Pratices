e8a15da1fc04f517c95feb7ca7beb6457cf52b17
fullzer4 <gabrielpelizzaro@gmail.com>
2023-05-08 12:57:49 +0000

    new

--- python/1_TwoSum/script.py
+++ python/1_TwoSum/script.py
@@ -1,5 +1,12 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]: int
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if(i != j and nums[i] + nums[j] == target):
+                    return [i,j]
+                
+        return []
+            
         
-        
-Solution.twoSum([2,7,11,15], 9)
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
--- typescript/1_TwoSum/index.ts
+++ typescript/1_TwoSum/index.ts
@@ -1,9 +1,15 @@
 
 function twoSum(nums: number[], target: number): number[] {
 
-
-
-    return []
+	for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [nums[i], nums[j]]
+            }
+        }
+    }
+    
+    return [];
 };
 
 twoSum([2,7,11,15], 9)
