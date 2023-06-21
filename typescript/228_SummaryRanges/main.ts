17674c5fd92816333c9013078bccdd5f771d2156
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-21 08:49:37 -0300

    resolved

--- typescript/228_SummaryRanges/main.ts
+++ typescript/228_SummaryRanges/main.ts
@@ -1,17 +1,25 @@
-let nums = [0,2,3,5,6,8,10];
+let nums = [0,2,3,4,5,6,8,10,11,12,13,16];
 
 var summaryRanges = function(nums: Array<number>) {
 
   let order: Array<string> = []   
-  let point: number = 0
   let i: number = 0
-  let Fnumber: number = 0
-  let first: number = nums[0];
+  let numbers: Array<number> = []
+  let isolate: number = 0
 
   for(i; i< nums.length; i++){
     if(nums[i] + 1 != nums[i + 1]){
-      order.push(`${nums[i]}`)
-      point++
+      isolate = nums[i]
+      if(numbers.length != 0){
+        console.log(`isolate: ${isolate}`)
+        console.log(`realizar ${numbers}`)
+        order.push(`${numbers[0]}->${isolate}`)
+      }else{
+        order.push(`${nums[i]}`)
+      }
+      numbers = []
+    } else{
+      numbers.push(nums[i])
     }
   }
   return order;
