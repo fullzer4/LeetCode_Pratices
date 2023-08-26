f8353edfa2c0b9f8c1d04a8bd1e0e20d3674672f
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-26 07:32:33 -0300

    new filter

---
+++ 30_Days_Pandas/4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = [[1, 3, 5, '2019-08-01'], [1, 3, 6, '2019-08-02'], [2, 7, 7, '2019-08-01'], [2, 7, 6, '2019-08-02'], [4, 7, 1, '2019-07-22'], [3, 4, 4, '2019-07-21'], [3, 4, 4, '2019-07-21']]
+Views = pd.DataFrame(data, columns=['article_id', 'author_id', 'viewer_id', 'view_date']).astype({'article_id':'Int64', 'author_id':'Int64', 'viewer_id':'Int64', 'view_date':'datetime64[ns]'})
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[(views["author_id"] == views["viewer_id"]) ]
+    return df
+
+article_views(Views)
