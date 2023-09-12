4dcd6bf1ff38d51b1098fd4baadeeb340eca2dd6
fullzer4 <gabrielpelizzaro@gmail.com>
2023-09-11 22:52:30 -0300

    pratices

---
+++ rust/src/problems/1.rs
@@ -0,0 +1,7 @@
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    return [3,3].to_vec();
+}
+
+fn main(){
+    two_sum([2,7,11,15], 9);
+}
--- typescript/1_TwoSum/index.ts
+++ typescript/1_TwoSum/index.ts
@@ -3,7 +3,7 @@ function twoSum(nums: number[], target: number): number[] {
 
 	for(let i = 0; i < nums.length; i++){
         for(let j = 0; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
+            if(i != j && nums[i] + nums[j] === target){
                 return [nums[i], nums[j]]
             }
         }
