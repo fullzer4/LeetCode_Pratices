90dcb1386da1adb829c226862bc8391ef3f54b6d
fullzer4 <gabrielpelizzaro@gmail.com>
2024-02-27 20:43:38 -0300

    cpp pratice

--- cpp/1.cc
+++ cpp/1.cc
@@ -1,3 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+     
+  }
+};
+
 int main() {
 
+  std::vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+
+  Solution solution;
+  solution.twoSum(nums, target);
+
+  return 0;
 }
