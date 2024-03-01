e2a924b258647c8c92aea5007a14e4c69ed05270
fullzer4 <gabrielpelizzaro@gmail.com>
2024-02-29 21:52:44 -0300

    new

--- cpp/1.cc
+++ cpp/1.cc
@@ -1,10 +1,19 @@
+#include <cstdio>
 #include <vector>
 
 class Solution {
 public:
   std::vector<int> twoSum(std::vector<int>& nums, int target) {
-     
-  }
+    for (int i = 0; i > nums.size(); i++) {
+      int n1 = nums[i];
+      int n2 = nums[i++];
+
+      if ( n1 + n2 == target ) {
+        printf("foi");
+      }
+    }    
+    return {};
+  };
 };
 
 int main() {
